15a3ec723d65d0d373bd24b3af68c15b0c2437aa
popo26 <mynameispopo26@gmail.com>
2022-03-25 11:55:36 +1300

    25march2022: lunch commit

--- 05_exceptions/05_04_else_database.py
+++ 05_exceptions/05_04_else_database.py
@@ -2,3 +2,22 @@
 # For example, you can revisit the course module about database interactions
 # and include a try/except/else statement based on what to do whether or not
 # the database connection can be established.
+
+
+import sqlalchemy
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+PASSWORD = os.getenv("PASSWORD")
+
+try:
+    engine = sqlalchemy.create_engine(f'mysql+pymysql://root:{PASSWORD}@localhost/sakila')
+    connection = engine.connect()
+    metadata = sqlalchemy.MetaData()
+    actor = sqlalchemy.Table('actor',metadata, autoload=True, autoload_with=engine)
+except:
+    print("Have you started MySQL service? \nHave you entered correct credentials? \nHave you add correct table name?\nSomething is not right.")
+else:
+    print(actor.columns.keys())
--- 05_exceptions/05_05_check_files.py
+++ 05_exceptions/05_05_check_files.py
@@ -4,4 +4,42 @@
 # with specific `except` statements, and continue to do the calculation
 # only if neither of them applies.
 
-file_name = 'integers.txt'
+
+##Ai not getting it
+
+file_name = "integer.txt"
+
+try:
+    with open(file_name, "r") as file:
+        num = file.readline()
+        first_num = num.strip()
+        print(first_num)
+        for n in file:
+            sum = 0
+            sum = int(first_num) + int(n)
+            print(sum)
+        
+except IOError:    
+    print("right file path?")
+       
+except ValueError:
+    print("right data type?")
+
+except:#Catching no error message
+    print("Not calculating correctly.")
+
+
+
+
+
+
+
+        
+
+
+    
+   
+    
+
+
+ 
