88ccc1c9e441c2454e58a5779a21e11dd26f9b6f
popo26 <mynameispopo26@gmail.com>
2022-04-05 14:07:21 +1200

    05apr2022: 06_testing completed

--- 06_testing/06_07_pytest_scraper.py
+++ 06_testing/06_07_pytest_scraper.py
@@ -1,2 +1,5 @@
 # Install `pytest` in a virtual environment and rewrite the test suite
 # for your web scraper using `pytest` instead of `unittest`.
+
+
+"""duplicate - 06_06_pytest_scraper.py"""
--- 06_testing/test_refactor_rescrape.py
+++ 06_testing/test_refactor_rescrape.py
@@ -10,11 +10,19 @@ import unittest
 import requests
 import refactor_rescrape
 from pathlib import Path
+from bs4 import BeautifulSoup
 
 class TestRefactorRescrape(TestCase):
     
     def setUp(self):
         self.URL = "http://books.toscrape.com/"
+        response = requests.get(self.URL)
+        data = response.text
+        self.soup = BeautifulSoup(data, "html.parser")
+        self.price_list_preparation1 = []
+        self.price_list_preparation2 = []
+        self.price_list_preparation3 = []
+    
 
 #can connect to API successfully
     def test_api_connection(self):
@@ -33,23 +41,68 @@ class TestRefactorRescrape(TestCase):
 
 #can write data to result.txt
     def test_write_data(self):
-        file = open("result.txt", "a")
+        file = open("result.txt", "a", encoding='utf-8')
         self.assertEqual(file.write("\n-------------Test line----------"), 33)
-        #Add a line to delete "-------------Test line----------"
         file.close()
+
+        """ Delete a line '-------------Test line----------'
+            Ref: https://www.geeksforgeeks.org/python-program-to-delete-specific-line-from-file/
+        """
+        with open("result.txt", "r") as file_read:
+            lines = file_read.readlines()
+            pointer = 1
+            with open("result.txt", "w") as file_write:
+                for line in lines:
+                    if pointer != 2242:
+                        file_write.write(line)
+                    pointer += 1  
 
 #can read data from text
     def test_read_data(self):
         file = open("result.txt", "r")
-        self.assertIn("-------------Test line----------", file.read())
+        self.assertIn("<!DOCTYPE html>", file.read())
 
 #can find book title text with CSS selectors
+    def test_scrape_price_to_list(self):
+        prices = self.soup.select(selector="p", class_="price_color")
+        for price in prices:
+            p = price.getText().strip("\n, In stock")
+            p2 = p.strip('')
+            self.price_list_preparation1.append(p2)
+        self.assertIn(p2, self.price_list_preparation1)
 
-#can find book price with CSS selectors
+#can stripping space
+    def test_strip_space(self):
+        for item in self.price_list_preparation1:
+            if item != '':
+                self.price_list_preparation2.append(item)
+        self.assertNotIn(" ", self.price_list_preparation2)
 
+#can strip Â
+    def test_strip_Â(self):
+        for item in self.price_list_preparation2:
+            final_price = item.split("Â")[1]
+            self.price_list_preparation3.append(final_price)
+        self.assertNotIn("Â", self.price_list_preparation3)
  
 
+#can find highest book price
+    def test_find_highest_price(self):
+        self.price_list_preparation3 = refactor_rescrape.find_price()
+        self.assertEqual(max(self.price_list_preparation3), "£57.25")
+ 
 
+#can find the index of the most expensive book
+    def test_find_book_title_most_expensive(self):
+        self.price_list_preparation3 = refactor_rescrape.find_price()
+        highest_price = max(self.price_list_preparation3)
+        self.assertEqual(self.price_list_preparation3.index(highest_price), 15)
+
+
+#can find the title of the most expensive book
+    def test_find_title_most_expensive_book(self):
+        book_names = refactor_rescrape.find_title()
+        self.assertEqual(book_names[15], "Our Band Could Be ...")
 
 
 
